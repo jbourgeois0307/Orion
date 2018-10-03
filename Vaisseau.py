e5e6a8385b96b403a474be6af7b306d07183143a
Barry Benjamin <1156246@decinfo.cvm>
2018-10-03 10:42:28 -0400

    modif missile attack

--- Vaisseau.py
+++ Vaisseau.py
@@ -53,9 +53,11 @@ class VaisseauGuerre(Vaisseau):
 		distance = math.sqrt(math.pow(self.x-self.cible.x,2)+math.pow(self.y-self.cible.y,2))
 		
 		if distance < self.range:
-			self.parent.parent.Vue.canevas.create_line(self.cible.x, self.cible.y, self.x, self.y, color="red")
+			missile = self.parent.parent.Vue.canevas.create_line(self.cible.x, self.cible.y, self.x, self.y, color="red")
 			self.cible.hp -= self.damage
 			tk.after(self.attackspeed, self.attack())
+			time.sleep(0.5)
+			del missile
 			
 class VaisseauTransport(Vaisseau):
 	def __init__(self, nom,x,y):
